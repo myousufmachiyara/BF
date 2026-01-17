b228a63c1baa8c9f0d3b6b6fdcb777b15a71eed1
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2026-01-17 15:26:46 +0500

    Update accounts_reports.blade.php

--- resources/views/reports/accounts_reports.blade.php
+++ resources/views/reports/accounts_reports.blade.php
@@ -172,7 +172,7 @@
 
                         {{-- ================= TOTAL FOOTER ================= --}}
                         @if (($key === 'receivables' || $key === 'payables') && count($reports[$key] ?? []) > 0)
-                            <tfoot class="table-default">
+                            <tfoot class="table-primary">
                                 <tr>
                                     <th>TOTAL</th>
                                     <th class="text-end">{{ number_format($totalAmount, 2) }}</th>
